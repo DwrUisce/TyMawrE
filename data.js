6490973b59591579cd050701a17c7709feece8dd
DwrUisce <105501146+DwrUisce@users.noreply.github.com>
2023-01-03 17:29:35 +0000

    v1
    v1

---
+++ data.js
@@ -0,0 +1,188 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-t-mawr-wybrnant-courtyard",
+      "name": "Tŷ Mawr Wybrnant Courtyard",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.544499484481964,
+        "pitch": -0.092070670671065,
+        "fov": 1.2933209261323464
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19998440691400354,
+          "pitch": 0.14843212120621097,
+          "rotation": 0,
+          "target": "1-powerhouse"
+        },
+        {
+          "yaw": -1.0050701765583643,
+          "pitch": -0.22285254660167908,
+          "rotation": 0,
+          "target": "2-river-intake"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.1837473764936384,
+          "pitch": -0.5935021580225772,
+          "title": "Welcome",
+          "text": "Welcome to a virtual tour of the Tŷ Mawr Wybrnant water turbine system. This system serves as a showcase of the potential for generating clean hydroelectricity for self-consumption in a cost-effective way.<div><br></div><div>To proceed with the tour, please make your way to the powerhouse and then the river intake scene by clicking on the ^ signs. Please explore around each scene and click on the 'i' signs for information on each scene.<br></div><div><br></div><div><p class=\"MsoNormal\"><span lang=\"EN-US\">This project is a collaboration between\nBangor University, Trinity College Dublin, and National Trust. It was made\npossible due to the support the ERDF Interreg Ireland-Wales Programme\n2014-2020, through the Dŵr Uisce Project.</span></p></div>"
+        },
+        {
+          "yaw": 2.957511482512367,
+          "pitch": -0.4262260647416092,
+          "title": "Tŷ Mawr Wybrnant",
+          "text": "The 16th century historical farmhouse of Tŷ Mawr Wybrnant is a renowned touristic site in Northern Wales managed by National Trust. In an effort to reduce the energy consumption related to lighting and heating of the building, a small hydropower scheme has been installed to generate about 4 kW from the water of the nearby Afon Wybrnant stream. Water is abstracted from the river at a higher elevation (+35 m) and is then carried via a 300 m pipeline up to the powerhouse. After generating electricity in the turbine, the water rejoins the stream next to the historical farmhouse."
+        }
+      ]
+    },
+    {
+      "id": "1-powerhouse",
+      "name": "Powerhouse",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.5049283698509583,
+        "pitch": 0.3210205265602468,
+        "fov": 1.2933209261323464
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7489656041033736,
+          "pitch": -0.07492553224769338,
+          "rotation": 0,
+          "target": "0-t-mawr-wybrnant-courtyard"
+        },
+        {
+          "yaw": -0.1152670625180896,
+          "pitch": -0.16717043659614106,
+          "rotation": 0,
+          "target": "2-river-intake"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.14003216837147114,
+          "pitch": 0.0710554654657809,
+          "title": "Powerhouse",
+          "text": "The powerhouse is made up of a steel frame cladded with local wood and serves to shelter the turbine and other equipment related to power regulation and generation."
+        },
+        {
+          "yaw": 0.843291711717649,
+          "pitch": 0.08696100640634263,
+          "title": "Turbine",
+          "text": "The turbine used for this system is a conventional water pump modified to operate in reverse as a turbine, with a maximum power output of around 4 kW.<div><br></div><div>Please continue the tour by clicking on the ^ sign on the shed to proceed to the river intake or click back on the second ^ sign to return to the main courtyard.<br></div>"
+        }
+      ]
+    },
+    {
+      "id": "2-river-intake",
+      "name": "River Intake",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.10103646097091357,
+        "pitch": 0.5250172758200566,
+        "fov": 1.2933209261323464
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.097668210027333,
+          "pitch": -0.1351307758695981,
+          "rotation": 0,
+          "target": "0-t-mawr-wybrnant-courtyard"
+        },
+        {
+          "yaw": -2.7329193164101806,
+          "pitch": -0.1470613148145734,
+          "rotation": 0,
+          "target": "1-powerhouse"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3971665726252418,
+          "pitch": 0.3231738628170575,
+          "title": "Weir",
+          "text": "The water can be extracted for use in the turbine thanks to this weir which raises the water level sufficiently to cause flow into the intake. The weir is also provided with a notch to allow the Hands-Off-Flow to pass along the stream independently from the hydro operations."
+        },
+        {
+          "yaw": 0.1332785457254566,
+          "pitch": 0.4587187783461193,
+          "title": "Screen and Intake",
+          "text": "If sufficient water flow is available in the stream, a portion of it passes through the metal screen, to remove larger debris, and then enters the 300 m long pipe leading to the powerhouse."
+        },
+        {
+          "yaw": -1.9633243965446034,
+          "pitch": -0.03346149082818961,
+          "title": "Thank You",
+          "text": "<span lang=\"EN-US\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-ascii-theme-font:minor-latin;\nmso-fareast-font-family:Calibri;mso-fareast-theme-font:minor-latin;mso-hansi-theme-font:\nminor-latin;mso-bidi-font-family:&quot;Times New Roman&quot;;mso-bidi-theme-font:minor-bidi;\nmso-ansi-language:EN-US;mso-fareast-language:EN-US;mso-bidi-language:AR-SA\">Once\nyou finish looking around this scene, it concludes our tour. Thank you for your\ninterest in our project!</span>"
+        }
+      ]
+    }
+  ],
+  "name": "Tŷ Mawr Wybrnant Turbine System",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
